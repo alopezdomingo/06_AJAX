af4fa3e2749d1c1d8d63c0331ee7881ed016402e
alopezdomingo <analopezdomingo@hotmail.com>
2018-07-06 09:14:39 +0200

    POO

--- 05_concesionario/concesionario.js
+++ 05_concesionario/concesionario.js
@@ -8,13 +8,26 @@ function verCoches(){
 	$.getJSON("concesionario.php",{m:m},muestra);
 	return false;
 }
-function muestra(resultado){
-	if  (resultado.modelo=='No')
+function muestra(ver){
+	$("#datos").html("");
+	if(typeof ver.matricula != "undefined")
+	{
+		$("<p>"+ver.matricula+"</p>").appendTo("#datos");
+		$("<p>"+ver.Marca+"</p>").appendTo("#datos");
+		/*	$("#datos").html(ver.Modelo);
+		$("#datos").html(ver.Precio);
+		$("#datos").html(ver.Km);*/
+	
+	}
+	else{
+		$("#datos").html("no existe ese vehículo");
+	}
+	/*if  (ver.modelo=='No')
 	{
 		$("#datos").html("Coche inexistente");
 	}
 	else{
-		$("#datos").html($reg['matricula']);
-	}
+		$("#datos").html($);
+	}*/
 }
   
--- 05_concesionario/concesionario.php
+++ 05_concesionario/concesionario.php
@@ -1,19 +1,36 @@
 <?php 
+	header("Content-type: application/json; charset=utf-8");
+
 	 $conexion=mysqli_connect("localhost","root","","concesionario");
-	 $matricula = $_REQUEST['matricula'];
+	 $matricula = $_REQUEST['m'];
 
 	 $registro=mysqli_query($conexion, "select * from coches where matricula='$matricula'") or die("ERROR DE CONSULTA");
+	 $cantidad = mysqli_num_rows($registro);
+	 if ($cantidad==0) {
+	 	# code...
+	 	echo "No existe vehículo";
+	 }else{
+	 	$reg=mysqli_fetch_array($registro);
+	 	
+	 	$matricula=$reg['matricula'];
+	 	$marca=$reg['marca'];
+	 	$modelo=$reg['modelo'];
+	 	$precio=$reg['precio'];
+	 	$km=$reg['km'];
 
-	 $reg=mysqli_fetch_array($registro);
-		echo "<h2>";
-		echo utf8_encode($reg['matricula']);
+	 	$array= array("matricula"=>$matricula,"Marca"=>$marca,"Modelo"=>$modelo, "Precio"=>$precio, "Km "=>$km);
+	 	echo json_encode($array);
+	 	
+	 	/*echo "<h2>";
+		echo utf8_encode($matricula);
 		echo "</h2><br>";
-		echo utf8_encode($reg['marca']);
+		echo utf8_encode($marca);
 		echo "<br>";
-		echo utf8_encode($reg['modelo']);
+		echo utf8_encode($modelo);
 		echo "<br>";
-		echo utf8_encode($reg['precio']);
+		echo utf8_encode($precio);
 		echo "<br>";
-		echo utf8_encode($reg['km']);
-		echo "<br>";
+		echo utf8_encode($km);
+		echo "<br>";*/
+	 }
  ?>
